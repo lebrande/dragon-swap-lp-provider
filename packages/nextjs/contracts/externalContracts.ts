ea8abdb04058fa802cdf80e852f283c067ec9bf1
Kuba <kuba@ipor.io>
2025-09-06 18:57:01 +0200

    Add USDC approve abi

--- packages/nextjs/contracts/externalContracts.ts
+++ packages/nextjs/contracts/externalContracts.ts
@@ -11,6 +11,38 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
  *   },
  * } as const;
  */
-const externalContracts = {} as const;
+const externalContracts = {
+  1329: {
+    USDC: {
+      address: "0xe15fC38F6D8c56aF07bbCBe3BAf5708A2Bf42392",
+      abi: [
+        {
+          inputs: [
+            {
+              internalType: "address",
+              name: "spender",
+              type: "address",
+            },
+            {
+              internalType: "uint256",
+              name: "value",
+              type: "uint256",
+            },
+          ],
+          name: "approve",
+          outputs: [
+            {
+              internalType: "bool",
+              name: "",
+              type: "bool",
+            },
+          ],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+      ],
+    },
+  },
+} as const;
 
 export default externalContracts satisfies GenericContractsDeclaration;
